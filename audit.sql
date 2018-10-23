f7ffaa74a92ff62fe7b06bc474ffceefcf5be5c4
Vamshi Surabhi <vamshi@34cross.in>
2018-10-23 18:29:38 +0530

    add a column to capture hasura's session

--- audit.sql
+++ audit.sql
@@ -46,6 +46,7 @@ CREATE TABLE audit.logged_actions (
     relid oid not null,
 
     session_user_name text,
+    hasura_session jsonb,
 
     action_tstamp_tx TIMESTAMP WITH TIME ZONE NOT NULL,
     action_tstamp_stm TIMESTAMP WITH TIME ZONE NOT NULL,
@@ -105,6 +106,7 @@ BEGIN
         TG_TABLE_NAME::text,                          -- table_name
         TG_RELID,                                     -- relation OID for much quicker searches
         session_user::text,                           -- session_user_name
+        current_setting('hasura.session')::jsonb,     -- session information from hasura
         current_timestamp,                            -- action_tstamp_tx
         statement_timestamp(),                        -- action_tstamp_stm
         clock_timestamp(),                            -- action_tstamp_clk
